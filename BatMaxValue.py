ec48b8799878c66445994f1fe23d420ef3e423b6
Gokhan Arman <gokhanarman89@gmail.com>
2023-01-01 01:49:04 +0300

    Some little updates added

--- BatMaxValue.py
+++ BatMaxValue.py
@@ -14,59 +14,92 @@ def optimize(x):
 
 # function to run the Yarasa algorithm
 def yarasa(pop_size, iterations, alpha, gama):
-  # initialize variables
-  pop = [0.0 for i in range(100)] # x values
-  fitness = [0.0 for i in range(100)] # fitness values
-  pulse_rate = [0.0 for i in range(100)] # pulse rates
-  loudness = [0.0 for i in range(100)] # loudness values
-  velocity = [0.0 for i in range(100)] # velocity values
+  #%% initialize variables
+  # Max 100 popsize
+  # 0 values for these variables in every iterations
+  
+  pop = [0.0 for i in range(pop_size)] # x values
+  fitness = [0.0 for i in range(pop_size)] # fitness values
+  pulse_rate = [0.0 for i in range(pop_size)] # pulse rates
+  loudness = [0.0 for i in range(pop_size)] # loudness values
+  velocity = [0.0 for i in range(pop_size)] # velocity values
+  
 
   # initialize other variables
-  fmin = 0.0
-  fmax = 0.001
-  best_solution = -1000000.0
+  # Frekans of bats. (We can choose what we want)
+  
+  fmin = 0.0      # Fi=[Fmin, Fmax] 
+  fmax = 0.001    # It can be Fmin=0.0 ve Fmax=1.0 
+  
+  best_solution = -1000000.0    # global best value
   random.seed()
   counter = 1
 
-  # calculate average loudness
-  avg_loudness = 0.0
+  #%% calculate average loudness
+  avg_loudness = 0.0            # Avarage Loudness of Bats
+  sum_all_loudness = 0.0
+  
   for i in range(pop_size):
-    loudness[i] = 0.001*(random.randint(0, 1000))
-    avg_loudness += loudness[i]
-  avg_loudness /= pop_size
+    loudness[i] = random.uniform(0, 1) # Random [0,1] For Loudness
+    sum_all_loudness = sum_all_loudness + loudness[i]        # sum all loudness for all bats
+  avg_loudness = sum_all_loudness / pop_size   # average loudness value           
 
-  # initialize pulse rates
+  #%% initialize pulse rates
   for i in range(pop_size):
-    pulse_rate[i] = 1 - 0.5*0.001*(random.randint(0, 1000))
+    pulse_rate[i] = random.uniform(0.5, 1) # Random [0.5,1] For Pulse Rates
 
-  # main loop
+  #%% main loop
   while counter <= iterations:
     # generate random population
     for i in range(pop_size):
-      pop[i] = 0.001*(random.randint(0, 10000)) # random x in range [0, 10]
+      pop[i] = random.uniform(0, 10) # random x in range [0, 10]
       fitness[i] = optimize(pop[i]) # calculate fitness
       if best_solution < fitness[i]: # update best solution
-        best_solution = fitness[i]
-        best_solution_x = pop[i]
+        best_solution = fitness[i]   # Global best function value
+        best_solution_x = pop[i]     # Global best x value
 
     # loop through population
+    # Two type search --> Local and random search
     for i in range(pop_size):
-      d = 0.001*(random.randint(0, 1000))
+      d = random.uniform(0, 1)
+      
       if d < pulse_rate[i]:
         # local search for i-th bat
-        pop[i] = best_solution_x
-        pop[i] += (random.uniform(-1.0, 1.0)*avg_loudness)
+        
+        # position of bat --> Xit = Xit-1                
+        pop[i] = best_solution_x  
+        
+        # position of bat --> Xi(t) = Xi(t-1) + €k . Aavg(t-1)
+        pop[i] = pop[i] + (random.uniform(-1.0, 1.0) * avg_loudness) # €k --> [-1, 1] range
+        
       else:
         # random search for i-th bat
         beta = random.uniform(0, 1)
-        fitness[i] = fmin + (fmax - fmin)*beta
-        velocity[i] = (velocity[i]*alpha) + (best_solution_x - pop[i])*(loudness[i]*beta) + (gama*pulse_rate[i]*(best_solution_x - pop[i]))
-        pop[i] += velocity[i]
+        
+        # Fi(t) = Fmin + (Fmax - Fmin).ui 
+        fitness[i] = fmin + (fmax - fmin)*beta 
+        
+        # Vi(t) = Vi(t-1) + (Xi(t-1) - Xg).Fi(t) 
+        velocity[i] = velocity[i] + (best_solution_x - pop[i]) * fitness[i]
+        
+        # Xi(t) = Xi(t-1) + Vi(t) 
+        
+        pop[i] = pop[i]  + velocity[i]
+        
     counter += 1
+    
+    for i in range(pop_size):
+        
+      # Ai(t+1) = alpha . Ai(t) 
+      loudness[i] = alpha * loudness[i]
+      
+      # Ri(t+1) = Rmaxt  . [1- 0.5. gama ^ iterasyon no]
+      pulse_rate[i] = 1.0 - 0.5 * gama**counter
+      
 
   # return best solution
-  return best_solution
+  return best_solution, best_solution_x
 
 # test the yarasa function
-result = yarasa(100, 1000, 0.9, 0.9)
+result = yarasa(20, 20, 0.95, 0.95)
 print("Best solution:", result)
